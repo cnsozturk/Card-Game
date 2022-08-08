60e21032d6afbc0e4e41657ec364d25261493736
Cansu Ozturk <cansuozturk@Cansu-MacBook-Pro.local>
2022-08-08 16:37:13 -0700

    first commit

---
+++ Q1-1/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Q1_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.butCom1 = new System.Windows.Forms.Button();
+            this.butCom2 = new System.Windows.Forms.Button();
+            this.butCom3 = new System.Windows.Forms.Button();
+            this.butCom4 = new System.Windows.Forms.Button();
+            this.butPlay4 = new System.Windows.Forms.Button();
+            this.butPlay3 = new System.Windows.Forms.Button();
+            this.butPlay2 = new System.Windows.Forms.Button();
+            this.butPlay1 = new System.Windows.Forms.Button();
+            this.butBet = new System.Windows.Forms.Button();
+            this.butDone = new System.Windows.Forms.Button();
+            this.butGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Computers Cards";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Players Cards";
+            // 
+            // butCom1
+            // 
+            this.butCom1.Location = new System.Drawing.Point(21, 53);
+            this.butCom1.Name = "butCom1";
+            this.butCom1.Size = new System.Drawing.Size(75, 118);
+            this.butCom1.TabIndex = 3;
+            this.butCom1.UseVisualStyleBackColor = true;
+            this.butCom1.Click += new System.EventHandler(this.butCom1_Click);
+            // 
+            // butCom2
+            // 
+            this.butCom2.Location = new System.Drawing.Point(120, 53);
+            this.butCom2.Name = "butCom2";
+            this.butCom2.Size = new System.Drawing.Size(75, 118);
+            this.butCom2.TabIndex = 4;
+            this.butCom2.UseVisualStyleBackColor = true;
+            // 
+            // butCom3
+            // 
+            this.butCom3.Location = new System.Drawing.Point(219, 53);
+            this.butCom3.Name = "butCom3";
+            this.butCom3.Size = new System.Drawing.Size(75, 118);
+            this.butCom3.TabIndex = 5;
+            this.butCom3.UseVisualStyleBackColor = true;
+            // 
+            // butCom4
+            // 
+            this.butCom4.Location = new System.Drawing.Point(318, 53);
+            this.butCom4.Name = "butCom4";
+            this.butCom4.Size = new System.Drawing.Size(75, 118);
+            this.butCom4.TabIndex = 6;
+            this.butCom4.UseVisualStyleBackColor = true;
+            // 
+            // butPlay4
+            // 
+            this.butPlay4.Location = new System.Drawing.Point(318, 267);
+            this.butPlay4.Name = "butPlay4";
+            this.butPlay4.Size = new System.Drawing.Size(75, 118);
+            this.butPlay4.TabIndex = 10;
+            this.butPlay4.UseVisualStyleBackColor = true;
+            // 
+            // butPlay3
+            // 
+            this.butPlay3.Location = new System.Drawing.Point(219, 267);
+            this.butPlay3.Name = "butPlay3";
+            this.butPlay3.Size = new System.Drawing.Size(75, 118);
+            this.butPlay3.TabIndex = 9;
+            this.butPlay3.UseVisualStyleBackColor = true;
+            // 
+            // butPlay2
+            // 
+            this.butPlay2.Location = new System.Drawing.Point(120, 267);
+            this.butPlay2.Name = "butPlay2";
+            this.butPlay2.Size = new System.Drawing.Size(75, 118);
+            this.butPlay2.TabIndex = 8;
+            this.butPlay2.UseVisualStyleBackColor = true;
+            // 
+            // butPlay1
+            // 
+            this.butPlay1.Location = new System.Drawing.Point(21, 267);
+            this.butPlay1.Name = "butPlay1";
+            this.butPlay1.Size = new System.Drawing.Size(75, 118);
+            this.butPlay1.TabIndex = 7;
+            this.butPlay1.UseVisualStyleBackColor = true;
+            // 
+            // butBet
+            // 
+            this.butBet.Location = new System.Drawing.Point(49, 416);
+            this.butBet.Name = "butBet";
+            this.butBet.Size = new System.Drawing.Size(75, 23);
+            this.butBet.TabIndex = 11;
+            this.butBet.Text = "Bet";
+            this.butBet.UseVisualStyleBackColor = true;
+            this.butBet.Click += new System.EventHandler(this.butBet_Click);
+            // 
+            // butDone
+            // 
+            this.butDone.Location = new System.Drawing.Point(190, 416);
+            this.butDone.Name = "butDone";
+            this.butDone.Size = new System.Drawing.Size(75, 23);
+            this.butDone.TabIndex = 12;
+            this.butDone.Text = "Done";
+            this.butDone.UseVisualStyleBackColor = true;
+            this.butDone.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // butGame
+            // 
+            this.butGame.Location = new System.Drawing.Point(88, 447);
+            this.butGame.Name = "butGame";
+            this.butGame.Size = new System.Drawing.Size(177, 23);
+            this.butGame.TabIndex = 13;
+            this.butGame.Text = "New Game";
+            this.butGame.UseVisualStyleBackColor = true;
+            this.butGame.Click += new System.EventHandler(this.butGame_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 482);
+            this.Controls.Add(this.butGame);
+            this.Controls.Add(this.butDone);
+            this.Controls.Add(this.butBet);
+            this.Controls.Add(this.butPlay4);
+            this.Controls.Add(this.butPlay3);
+            this.Controls.Add(this.butPlay2);
+            this.Controls.Add(this.butPlay1);
+            this.Controls.Add(this.butCom4);
+            this.Controls.Add(this.butCom3);
+            this.Controls.Add(this.butCom2);
+            this.Controls.Add(this.butCom1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button butCom1;
+        private System.Windows.Forms.Button butCom2;
+        private System.Windows.Forms.Button butCom3;
+        private System.Windows.Forms.Button butCom4;
+        private System.Windows.Forms.Button butPlay4;
+        private System.Windows.Forms.Button butPlay3;
+        private System.Windows.Forms.Button butPlay2;
+        private System.Windows.Forms.Button butPlay1;
+        private System.Windows.Forms.Button butBet;
+        private System.Windows.Forms.Button butDone;
+        private System.Windows.Forms.Button butGame;
+    }
+}
+
---
+++ Q1-1/Form1.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1_1
+{
+    public partial class Form1 : Form
+    {
+        Random rndGen = new Random();
+        int i = 0;
+        double sum = 0.0;
+        double sumComp = 0.0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void butCom1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void butBet_Click(object sender, EventArgs e)
+        {
+            string[] comp = new string[4];
+            string[] usr = new string[4];
+
+            int NumGenerator()
+            {
+                
+                int p = rndGen.Next(1, 14);
+                return p;
+
+            }
+
+            string m;
+
+            string CheckNumber (int j)
+                        {
+                         
+                if (j <= 10)
+                {
+                    string v = j.ToString();
+                    m = v;
+                }
+
+                else if (j == 11)
+                {
+                    m = "J";
+                }
+                else if (j == 12)
+                {
+                    m = "Q";
+                }
+                else if (j == 13)
+                {
+                    m = "K";
+                }
+                else  m=j.ToString();
+
+                return m;
+
+               
+
+            }
+                 
+         
+           
+                int t = NumGenerator();
+                int n = NumGenerator();
+                usr[i] = t.ToString();
+                comp[i] = n.ToString();
+                comp[i] = CheckNumber(n);
+                usr[i]=CheckNumber(t);
+         
+
+                if (i == 0)
+                {
+                
+                butPlay1.Text = usr[0].ToString();
+                    butCom1.Text = comp[0].ToString();
+                }
+
+
+                if (i == 1)
+                {
+                    butPlay2.Text = usr[1].ToString();
+                    butCom2.Text = comp[1].ToString();
+                }
+                if (i == 2)
+                {
+                    butCom3.Text = comp[2].ToString();
+                    butPlay3.Text = usr[2].ToString();
+                }
+                if (i == 3)
+
+                {
+                    butCom4.Text = comp[3].ToString();
+                    butPlay4.Text = comp[3].ToString();
+                Array.Clear(usr, 0, usr.Length);
+                Array.Clear(comp, 0, comp.Length);
+              
+
+            }
+
+
+          
+
+            if (t > n && t > 10)
+            {
+
+                sum = +0.5;
+            }
+            else if (t > n && t <= 10)
+            { sum = sum + t; }
+            if (n > t && n > 10)
+            {
+                sumComp = +0.5;
+            }
+            else if (n > t && n < 10)
+            {
+                sumComp = n + sumComp;
+            }
+
+
+            if (i == 3)
+            {
+                CalculateSum(sum, sumComp);
+
+
+            }
+            i++;
+            if (i == 4)
+            {
+
+                startOver();
+            }
+       
+          
+
+        }
+
+       public void  CalculateSum(double sum, double sumComp)
+        {
+            if (sum > sumComp && sum < 10)
+            {
+
+                MessageBox.Show("Blast!");
+
+               
+            }
+            else if (sum > sumComp && sum > 10)
+            {
+
+                MessageBox.Show("You Won");
+
+              
+            }
+            else if (sumComp > sum && sumComp < 10)
+            {
+                MessageBox.Show("Blast!");
+
+            }
+            else MessageBox.Show("Computer won");
+
+         
+
+
+        }
+
+        public int  startOver()
+        {
+     
+        double sum = 0.0;
+        double sumComp = 0.0;
+            return i = 0;
+        }
+
+
+        private void butGame_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("New Game!");
+            butCom1.Text = " ";
+            butCom2.Text = "  ";
+            butCom3.Text = "  ";
+            butCom4.Text = " ";
+            butPlay1.Text = "  ";
+            butPlay2.Text = "  ";
+            butPlay3.Text = "  ";
+            butPlay4.Text = "";
+
+          
+
+          
+
+        }
+    }
+    }
+
